8052422b0cedeef940c8e2451b5172d88ad1650c
cswmm <nathandurrant726@gmail.com>
2024-09-16 13:47:56 -0700

    Update measure file
    Refactored constant measurement to match use in solver

--- measure.py
+++ measure.py
@@ -5,9 +5,29 @@ source = [c.upper() for c in f.read() if c.isalpha()]
 size = len(source)
 E = np.zeros((26, 26))
 
+# count letter frequency
+count = {}
+for ch in source:
+    count[ch] = count.get(ch, 0) + 1
+
+for k, v in count.items():
+    count[k] = v / size
+
+countByFreq = dict(sorted(count.items(), key=lambda x: x[1], reverse=True))
+lettersByFreq = ''
+for ch in countByFreq.keys():
+    lettersByFreq += ch
+
+# get letters by frequency to be used as a const
+print(lettersByFreq)
+
+print(countByFreq)
+
+# create digram matrix to be used in const
+
 numDigrams = 0
 for i in range(size - 1):
-    E[ord(source[i]) - 65][ord(source[i+1]) - 65] += 1
+    E[lettersByFreq.find(source[i])][lettersByFreq.find(source[i + 1])] += 1
     numDigrams += 1
 
 for i in range(26):
